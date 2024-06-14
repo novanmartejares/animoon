19497b67c74b68dfca856399bdb9ddbdf80c02f7
rishabh-star <risingmerta@gmail.com>
2024-06-14 15:31:51 +0530

    first commit

---
+++ src/app/watch/[id]/error.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import Error from '@/component/AnimeNotFound/Error'
+
+const error = () => {
+  return (
+    <div>
+      <Error/>
+    </div>
+  )
+}
+
+export default error
