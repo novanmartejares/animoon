a47cdb147a7075dce70ca36090b13ea5dff07b9c
rishabh-star <risingmerta@gmail.com>
2024-06-15 00:14:33 +0530

    first commit

--- src/component/SpecialErr/SpecialErr.jsx
+++ src/component/SpecialErr/SpecialErr.jsx
@@ -7,7 +7,7 @@ export default function Error() {
       style={{ marginTop: "65px" }}
       className="gogoanime-error d-flex-fd-column a-center j-center"
     >
-      <Image src={'https://cdn.otakugifs.xyz/gifs/cool/d2776011d23a58d9.gif'} alt="error"/>
+      <img src={'https://cdn.otakugifs.xyz/gifs/cool/d2776011d23a58d9.gif'} alt="error"/>
       <h2>So,you have decided to start a new anime...;)</h2>
     </div>
   );
