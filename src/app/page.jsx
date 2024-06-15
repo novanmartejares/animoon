473b07d9651c794bc886feba53c3c8b8d44d9ce4
rishabh-star <risingmerta@gmail.com>
2024-06-15 12:53:29 +0530

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,96 +5,93 @@ const DynamicHome = dynamic(() => import("@/app/home/home"), {
 });
 
 export default async function page() {
-  let data = []
+  let data = [];
   try {
     const resp = await fetch(
       "https://aniwatch-api-8fti.onrender.com/anime/home",
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
     data = await resp.json();
   } catch (error) {
-    data = []
+    data = [];
   }
 
-
   // categories -> "most-favorite", "most-popular", "subbed-anime", "dubbed-anime", "recently-updated", "recently-added", "top-upcoming", "top-airing", "movie", "special", "ova", "ona", "tv", "completed"
-  let dataAiring = []
+  let dataAiring = [];
   try {
     const resp = await fetch(
       "https://aniwatch-api-8fti.onrender.com/anime/top-airing?page=1",
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
     dataAiring = await resp.json();
   } catch (error) {
-    dataAiring = []
+    dataAiring = [];
   }
 
-  let dataFavourite = []
+  let dataFavourite = [];
   try {
     const resp = await fetch(
       "https://aniwatch-api-8fti.onrender.com/anime/most-favorite?page=1",
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
     dataFavourite = await resp.json();
   } catch (error) {
-    dataFavourite = []
+    dataFavourite = [];
   }
 
-
-  let dataPopular = []
+  let dataPopular = [];
   try {
     const resp = await fetch(
       "https://aniwatch-api-8fti.onrender.com/anime/most-popular?page=1",
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
     dataPopular = await resp.json();
   } catch (error) {
-    dataPopular = []
+    dataPopular = [];
   }
 
-
-  let dataComp = []
+  let dataComp = [];
   try {
     const resp = await fetch(
       "https://aniwatch-api-8fti.onrender.com/anime/most-popular?page=1",
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
     dataComp = await resp.json();
   } catch (error) {
-    dataComp = []
+    dataComp = [];
   }
 
-  let dataLatest = []
+  let dataLatest = [];
   try {
     const resp = await fetch(
       "https://aniwatch-api-8fti.onrender.com/anime/recently-updated?page=1",
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
     dataLatest = await resp.json();
   } catch (error) {
-    dataLatest = []
+    dataLatest = [];
   }
 
-  let dataNew = []
+  let dataNew = [];
   try {
     const resp = await fetch(
       "https://aniwatch-api-8fti.onrender.com/anime/recently-added?page=1",
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
     dataNew = await resp.json();
   } catch (error) {
-    dataNew = []
+    dataNew = [];
   }
 
-  let dataUpcoming = []
+  let dataUpcoming = [];
   try {
     const resp = await fetch(
       "https://aniwatch-api-8fti.onrender.com/anime/top-upcoming?page=1",
-      { cache: "no-store" }
+      { next: { revalidate: 3600 } }
     );
     dataUpcoming = await resp.json();
   } catch (error) {
-    dataUpcoming = []
+    dataUpcoming = [];
   }
 
   return (
