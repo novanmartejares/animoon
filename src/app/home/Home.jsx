f8a6d107b7d66496432960d7bb2b925265361d6a
rishabh-star <risingmerta@gmail.com>
2024-11-09 23:20:23 +0530

    first commit

--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -5,20 +5,21 @@ import Hero from "@/component/Hero/hero";
 import Trending from "@/component/Trending/Trending";
 import Featured from "@/component/Featured/Featured";
 import MainContainer from "@/component/MainContainer/MainContainer";
-// import Henpro from "@/component/Henpro/Henpro";
 import LoadingSpinner from "@/component/loadingSpinner";
 import Script from "next/script";
 
 const Home = (props) => {
   const [isLoading, setIsLoading] = useState(false);
+
   const IsLoading = (data) => {
     if (data) {
       setIsLoading(true);
       setTimeout(() => {
         setIsLoading(false);
-      }, [20000]);
+      }, 20000); // Adjust the timeout as needed
     }
   };
+
   return (
     <>
       {isLoading ? (
@@ -27,18 +28,20 @@ const Home = (props) => {
         <div>
           <Hero data={props.data.spotlightAnimes} IsLoading={IsLoading} />
           {/* <Henpro/> */}
-          <div style="text-align: center; margin: 20px 0;">
-            {/* <!-- Embed the iframe to show the ad --> */}
+          
+          {/* Banner Ad Section */}
+          <div style={{ textAlign: "center", margin: "20px 0" }}>
             <iframe
               src="//disgustingmad.com/3e5c0db0e54f3f6872ff8546641e31c0/invoke.js"
               width="468"
               height="60"
-              style="border: none; overflow: hidden;"
+              style={{ border: "none", overflow: "hidden" }}
               scrolling="no"
-              frameborder="0"
+              frameBorder="0"
               title="Banner Ad"
             ></iframe>
           </div>
+
           <Trending data={props.data.trendingAnimes} IsLoading={IsLoading} />
           <Share ShareUrl={props.ShareUrl} />
           <Featured
