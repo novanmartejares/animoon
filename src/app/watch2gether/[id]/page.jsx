dbca1639f0851314bbaceadd673cc726387c3c26
rishabh-star <risingmerta@gmail.com>
2024-11-29 15:06:03 +0530

    first commit

--- src/app/watch2gether/[id]/page.jsx
+++ src/app/watch2gether/[id]/page.jsx
@@ -5,7 +5,7 @@ export default async function page({ params, searchParams }) {
   let data = [];
   if (searchParams.animeId) {
     const resp = await fetch(
-      `https://hianimes.animoon.me/anime/info?id=${searchParams.animeId}`
+      `https://hianimes.vercel.app/anime/info?id=${searchParams.animeId}`
     );
     data = await resp.json();
   }
